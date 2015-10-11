cd8c1b27e8ccbb5e8e62db839e5eb33690fe8ec6
Matt Koski <mkoski6@gmail.com>
2015-10-11 14:50:05 -0400

    adds beginnings of timeline toggling

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -75,3 +75,76 @@ $('form.create-decade').on('submit', function(e){
   var newComment = $(this).find("#decade-comment").val();
   decades.create({comment: newComment});
 });
+
+function show2010(){
+  $("#2010").click(function(){
+      $("#1900player").hide();
+      $("#1910player").hide();
+      $("#1920player").hide();
+      $("#1930player").hide();
+      $("#1940player").hide();
+      $("#1950player").hide();
+      $("#1960player").hide();
+      $("#1970player").hide();
+      $("#1980player").hide();
+      $("#1990player").hide();
+      $("#2000player").hide();
+  });
+  $("#2010").click(function(){
+      $("#2010player").show();
+  });
+}
+
+function show1990(){
+    $("#1990").click(function(){
+        $("#1900player").hide();
+        $("#1910player").hide();
+        $("#1920player").hide();
+        $("#1930player").hide();
+        $("#1940player").hide();
+        $("#1950player").hide();
+        $("#1960player").hide();
+        $("#1970player").hide();
+        $("#1980player").hide();
+        $("#2000player").hide();
+        $("#2010player").hide();
+    });
+    $("#1990").click(function(){
+        $("#1990player").show();
+    });
+  }
+
+  function show2000(){
+      $("#2000").click(function(){
+          $("#1900player").hide();
+          $("#1910player").hide();
+          $("#1920player").hide();
+          $("#1930player").hide();
+          $("#1940player").hide();
+          $("#1950player").hide();
+          $("#1960player").hide();
+          $("#1970player").hide();
+          $("#1980player").hide();
+          $("#1990player").hide();
+          $("#2010player").hide();
+      });
+      $("#2000").click(function(){
+          $("#2000player").show();
+      });
+    }
+
+
+$(document).ready(function(){
+  // show1900();
+  // show1910();
+  // show1920();
+  // show1930();
+  // show1940();
+  // show1950();
+  // show1960();
+  // show1970();
+  // show1980();
+  show1990();
+  show2000();
+  show2010();
+});
